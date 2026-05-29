76c6aa22da46a3de91e8d3b54f96e177bfef72dd
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-29 18:26:48 +0000

    Clean up build.gradle.kts by removing unused settings
    Removed unused configurations and dependencies from build.gradle.kts.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,42 +25,12 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
-        }
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
-            minifyEnabled = true
-            shrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
-
-        debug {
-            signingConfig = signingConfigs.getByName("debug")
-            debuggable = true
-            minifyEnabled = false
-        }
-    }
-
-    packagingOptions {
-        exclude("META-INF/proguard/androidx-*.pro")
-        exclude("META-INF/DEPENDENCIES")
-        exclude("META-INF/LICENSE")
-        exclude("META-INF/NOTICE")
-    }
-
-    lintOptions {
-        disable("InvalidPackage")
-        disable("MissingDimensionProductFlavor")
     }
 }
 
@@ -69,22 +39,9 @@ flutter {
 }
 
 dependencies {
-    implementation("io.agora.rtc:full-rtc-sdk:4.2.5")
-
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-core")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
-    implementation("com.google.firebase:firebase-messaging-ktx")
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core:1.12.0")
-    implementation("androidx.preference:preference:1.2.1")
-
-    implementation("com.google.android.material:material:1.11.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
