64e9941e1ae7feb1a9b5ddb400d5b92d408b8a16
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-29 18:44:29 +0000

    Remove JitPack repository from settings.gradle.kts
    Removed JitPack repository from settings.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,10 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
 include(":app")
-
-rootProject.name = "crux"
