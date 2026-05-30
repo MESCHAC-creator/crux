930ee4a10a24e282d5bef1c84ce0987cc3ccb7de
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-30 11:07:14 +0000

    Update settings.gradle.kts to include Flutter SDK
    Load Flutter SDK path from local.properties and include Flutter tools build.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,15 @@
 pluginManagement {
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
+        }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -6,12 +17,10 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "9.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
 }
 
 include(":app")
