d9455a152c6ea8465de5608a2242c9546097bd2b
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-30 16:41:11 +0000

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,34 +1,13 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     id("com.google.gms.google-services")
+    id("kotlin-android")
 }
 
 android {
     namespace = "com.example.crux"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.crux"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-        multiDexEnabled = true
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            isDebuggable = true
-        }
-    }
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -39,8 +18,46 @@ android {
         jvmTarget = "11"
     }
 
+    defaultConfig {
+        applicationId = "com.example.crux"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = false
+        }
+    }
+
     lint {
-        disable += listOf("MissingDimensionRegistration", "InvalidPackage")
+        disable += listOf(
+            "MissingDimensionRegistration",
+            "InvalidPackage",
+            "MissingTranslation",
+            "ExtraTranslation"
+        )
+    }
+
+    packagingOptions {
+        exclude("META-INF/proguard/androidx-*.pro")
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/LICENSE")
+        exclude("META-INF/LICENSE.txt")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/NOTICE.txt")
     }
 }
 
@@ -49,13 +66,23 @@ flutter {
 }
 
 dependencies {
+    // Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    
+    // Firebase
     implementation("com.google.firebase:firebase-core")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-analytics")
+    
+    // Android Support
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.multidex:multidex:2.0.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 }
