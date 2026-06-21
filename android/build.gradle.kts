032ece287b081e0a33ab5fdfc2e81b884ae41eec
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-06-21 06:46:10 +0000

    Update and rename build.gradle to build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,41 @@
+buildscript {
+    val kotlinVersion = "1.8.20"
+    
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+rootProject.buildDir = file("../build")
+
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
